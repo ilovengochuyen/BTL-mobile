4ca5d3640fa9d6dcc8bef75e425466e44e4b9aa2
Đinh Ngọc Vân <dinhngocvan060201@gmail.com>
2022-05-16 15:44:02 +0700

    add model
    comic model and chapter model

---
+++ lib/model/ChapterModel.dart
@@ -0,0 +1,9 @@
+class ChapterModel {
+  String name = '';
+  String image = '';
+  String description = '';
+  //List<dynamic> chapterpages
+
+  ChapterModel({required this.image,
+    required this.name, required this.description});
+}
---
+++ lib/model/ComicModel.dart
@@ -0,0 +1,10 @@
+class ComicModel {
+  String name = '';
+  String image = '';
+  String genres = '';
+  String description = '';
+  String author = '';
+
+  ComicModel({required this.image, required this.name,
+              required this.description, required this.genres, required this.author});
+}
