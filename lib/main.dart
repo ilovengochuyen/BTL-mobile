ec221ae5898b476b9007dded42c64d0a6e86ff10
Đinh Ngọc Vân <dinhngocvan060201@gmail.com>
2022-05-16 16:49:53 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:manga/providers/comic_provider.dart';
 import 'package:manga/screens/screens.dart';
+import 'package:provider/provider.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ComicProvider>(
+        create: (context)=>ComicProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: 'start',
+          title: 'manga',
+          routes: {
+            'login': (context) => LoginPage(),
+            'register': (context) => myRegister(),
+            'forgot': (context) => resetPassword(),
+            'signup': (context) => SignupPage(),
+            'start': (context) => StartScreen(),
+            'comic_home_screen': (context) => ComicHomeScreen(),
+          },
+        )
+        );
+    //);
+  }
+}
+
+/*
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -19,4 +54,4 @@ void main() async {
       },
     ),
   );
-}
+}*/
